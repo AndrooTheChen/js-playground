46e78bda4f1324c24926784f6ff01c3c8d700e5c
Andrew Chen <andrew.chen@discordapp.com>
2023-03-25 23:19:53 -0700

    ios hack

---
+++ rosa/src/components/nav.svelte
@@ -0,0 +1,4 @@
+<nav class="flex justify-center w-full">
+    <a class="text-white mx-4 text-lg" href="/">Home</a>
+    <a class="text-white mx-4 text-lg" href="/aboot">About</a>
+</nav>
---
+++ rosa/src/routes/aboot/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+    the aboot page
+</svelte:head>
+<ion-card>
+    <img class="w-60 h-60 object-cover mx-auto object-center" alt="moar rosa" src="https://images2.alphacoders.com/661/661134.jpg" />
+    <ion-card-header>
+      <ion-card-title>even moar rosa</ion-card-title>
+      <ion-card-subtitle>i don't know what to write here</ion-card-subtitle>
+    </ion-card-header>
+  
+    <ion-card-content>
+      imagine having things to write here.
+    </ion-card-content>
+  </ion-card>
