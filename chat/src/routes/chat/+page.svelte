02518085cb30373eac422e13a8688173da71c8b9
Andrew Chen <andrew.chen@discordapp.com>
2023-03-27 01:05:50 -0700

    add menu interface

--- chat/src/routes/chat/+page.svelte
+++ chat/src/routes/chat/+page.svelte
@@ -41,32 +41,59 @@
 </script>
 
 <IonPage>
-    <ion-header>
+    <!-- <ion-header>
       <ion-toolbar color="primary">
         <ion-title>Chat App</ion-title>
       </ion-toolbar>
-    </ion-header>
-    <ion-content>
-        <div class="flex flex-col h-full overflow-y-auto">
-            <div class="overflow-y-auto flex flex-col-reverse" style="height: calc(100vh - 100px);">
-                <ion-list>
-                {#each messages as message}
-                <ion-item lines="none" class={message.role === 'user' ? 'ion-justify-content-end' : 'ion-justify-content-start'}>
-                    <ion-label class="ion-text-wrap">{message.content}</ion-label>
-                </ion-item>
-                {/each}
-                </ion-list>
-            </div>
-            <ion_footer class="fixed bottom-0 w-full h-12">
-                <form on:submit|preventDefault={chat} class="ion-padding">
-                    <ion-item>
-                      <ion-input type="text" placeholder="Type your message" value={newMessage} on:input={(e) => newMessage = e.target.value}></ion-input>
-                      <ion-button type="submit" fill="solid" slot="end">Send</ion-button>
+    </ion-header> -->
+
+    <ion-split-pane content-id="main">
+        <ion-menu content-id="main">
+          <ion-header>
+            <ion-toolbar color="tertiary">
+              <ion-title>Menu</ion-title>
+            </ion-toolbar>
+          </ion-header>
+          <ion-content class="ion-padding">
+            Menu Content
+          </ion-content>
+        </ion-menu>
+      
+        <div class="ion-page" id="main">
+          <ion-header>
+            <ion-toolbar>
+              <ion-title>Buddy</ion-title>
+              <ion-buttons slot="start">
+                <ion-menu-button></ion-menu-button>
+              </ion-buttons>
+            </ion-toolbar>
+          </ion-header>
+          <ion-content>
+            <div class="flex flex-col h-full overflow-y-auto">
+                <div class="overflow-y-auto flex flex-col-reverse" style="height: calc(100vh - 100px);">
+                    <ion-list>
+                    {#each messages as message}
+                    <ion-item lines="none" class={message.role === 'user' ? 'ion-justify-content-end' : 'ion-justify-content-start'}>
+                        <ion-label class="ion-text-wrap">{message.content}</ion-label>
                     </ion-item>
-                  </form>
-              </ion_footer>
+                    {/each}
+                    </ion-list>
+                </div>
+                <ion_footer class="fixed bottom-0 w-full h-12">
+                    <form on:submit|preventDefault={chat} class="ion-padding">
+                        <ion-item>
+                          <ion-input type="text" placeholder="Type your message" value={newMessage} on:input={(e) => newMessage = e.target.value}></ion-input>
+                          <ion-button type="submit" fill="solid" slot="end">Send</ion-button>
+                        </ion-item>
+                      </form>
+                  </ion_footer>
+            </div>
+        </ion-content>
         </div>
-    </ion-content>
+      </ion-split-pane>
+
+
+
 
 
     <!-- this should technically be better but breaks it and adds a second outer scrollbar, very jank.  -->
